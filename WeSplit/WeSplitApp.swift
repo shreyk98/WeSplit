babcb715993caafe9a24edf2d5ec691e8f055c5f
shrey kakkar <shrey.kakkar@nortonlifelock.com>
2021-02-26 13:40:48 -0800

    Add Xcode project

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shrey Kakkar on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
